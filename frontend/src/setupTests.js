a0e6227389b98d1e2293333e1521c8472d26783a
Assyl <assyl@uni.minerva.edu>
2026-04-17 02:30:50 -0700

    test: add React Testing Library (RTL) tests for auth forms, browse page, item detail, and protected routes

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,3 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// react-router v7 requires TextEncoder/TextDecoder (not in jsdom by default)
+const { TextEncoder, TextDecoder } = require('util');
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
